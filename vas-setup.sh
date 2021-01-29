5cca23fc062247eb0e91066f897b4e77848a66cc
sudo-phantom <sudophantom@gmail.com>
2021-01-29 02:37:04 -0700

    Update vas-setup.sh
    changing apt to apt-get

--- vas-setup.sh
+++ vas-setup.sh
@@ -1,16 +1,16 @@
 
 apt update;
-apt install gcc pkg-config sudo libssh-gcrypt-dev libgnutls28-dev  libglib2.0-dev libpcap-dev libgpgme-dev bison libksba-dev libsnmp-dev libgcrypt20-dev redis-server git python-pip cmake;
+apt-get install gcc pkg-config sudo libssh-gcrypt-dev libgnutls28-dev  libglib2.0-dev libpcap-dev libgpgme-dev bison libksba-dev libsnmp-dev libgcrypt20-dev redis-server git python-pip cmake -y;
 sleep 5;
 echo "pausing for apt";
-apt install bison cmake gcc gcc-mingw-w64 heimdal-dev libgcrypt20-dev libglib2.0-dev libgnutls28-dev libgpgme-dev libhiredis-dev libksba-dev libmicrohttpd-dev git libpcap-dev libpopt-dev libsnmp-dev libsqlite3-dev libssh-gcrypt-dev xmltoman libxml2-dev perl-base pkg-config python3-paramiko python3-setuptools uuid-dev curl redis doxygen libical-dev python-polib gnutls-bin;
+apt-get install bison cmake gcc gcc-mingw-w64 heimdal-dev libgcrypt20-dev libglib2.0-dev libgnutls28-dev libgpgme-dev libhiredis-dev libksba-dev libmicrohttpd-dev git libpcap-dev libpopt-dev libsnmp-dev libsqlite3-dev libssh-gcrypt-dev xmltoman libxml2-dev perl-base pkg-config python3-paramiko python3-setuptools uuid-dev curl redis doxygen libical-dev python-polib gnutls-bin -y;
 sleep 5;
 echo "pausing for apt";
 clear;
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -;
 sleep .5;
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list;
-apt install yarn ;
+apt-get install yarn -y;
 clear;
 ################################
 cd /tmp;
