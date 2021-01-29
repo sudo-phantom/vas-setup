b84e4de7234d14994155e6fc45eca25284f16f41
sudo-phantom <sudophantom@gmail.com>
2021-01-29 01:17:24 -0700

    Update vas-setup.sh

--- vas-setup.sh
+++ vas-setup.sh
@@ -1,16 +1,17 @@
-#curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -;
-#sleep .5;
-#echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list;
+
 apt update;
-cwd=$(pwd);
-apt install gcc pkg-config libssh-gcrypt-dev libgnutls28-dev  libglib2.0-dev libpcap-dev libgpgme-dev bison libksba-dev libsnmp-dev libgcrypt20-dev redis-server git python-pip cmake -y;
+apt install gcc pkg-config libssh-gcrypt-dev libgnutls28-dev  libglib2.0-dev libpcap-dev libgpgme-dev bison libksba-dev libsnmp-dev libgcrypt20-dev redis-server git python-pip cmake;
 sleep 5;
 echo "pausing for apt";
-apt install bison cmake gcc gcc-mingw-w64 heimdal-dev libgcrypt20-dev libglib2.0-dev libgnutls28-dev libgpgme-dev libhiredis-dev libksba-dev libmicrohttpd-dev git libpcap-dev libpopt-dev libsnmp-dev libsqlite3-dev libssh-gcrypt-dev xmltoman libxml2-dev perl-base pkg-config python3-paramiko python3-setuptools uuid-dev curl redis doxygen libical-dev python-polib gnutls-bin -y;
+apt install bison cmake gcc gcc-mingw-w64 heimdal-dev libgcrypt20-dev libglib2.0-dev libgnutls28-dev libgpgme-dev libhiredis-dev libksba-dev libmicrohttpd-dev git libpcap-dev libpopt-dev libsnmp-dev libsqlite3-dev libssh-gcrypt-dev xmltoman libxml2-dev perl-base pkg-config python3-paramiko python3-setuptools uuid-dev curl redis doxygen libical-dev python-polib gnutls-bin;
 sleep 5;
 echo "pausing for apt";
 clear;
-apt install yarn -y;
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -;
+sleep .5;
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list;
+apt install yarn ;
+cwd=$(pwd);
 cd $cwd/tools;
 sleep .5;
 clear;
