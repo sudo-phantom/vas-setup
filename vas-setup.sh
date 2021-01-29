9adaedd8894ac6e7f4bdf4c264946338cec52615
sudo-phantom <sudophantom@gmail.com>
2021-01-28 18:42:54 -0700

    Update vas-setup.sh
    added -y and created a directory to work out of incase user is root

--- vas-setup.sh
+++ vas-setup.sh
@@ -1,11 +1,13 @@
 apt update;
-apt-get install gcc pkg-config libssh-gcrypt-dev libgnutls28-dev  libglib2.0-dev libpcap-dev libgpgme-dev bison libksba-dev libsnmp-dev libgcrypt20-dev redis-server git python-pip cmake;
-apt install bison cmake gcc gcc-mingw-w64 heimdal-dev libgcrypt20-dev libglib2.0-dev libgnutls28-dev libgpgme-dev libhiredis-dev libksba-dev libmicrohttpd-dev git libpcap-dev libpopt-dev libsnmp-dev libsqlite3-dev libssh-gcrypt-dev xmltoman libxml2-dev perl-base pkg-config python3-paramiko python3-setuptools uuid-dev curl redis doxygen libical-dev python-polib gnutls-bin;
+cd ~;
+mkdir tools;
+apt-get install gcc pkg-config libssh-gcrypt-dev libgnutls28-dev  libglib2.0-dev libpcap-dev libgpgme-dev bison libksba-dev libsnmp-dev libgcrypt20-dev redis-server git python-pip cmake -y;
+apt install bison cmake gcc gcc-mingw-w64 heimdal-dev libgcrypt20-dev libglib2.0-dev libgnutls28-dev libgpgme-dev libhiredis-dev libksba-dev libmicrohttpd-dev git libpcap-dev libpopt-dev libsnmp-dev libsqlite3-dev libssh-gcrypt-dev xmltoman libxml2-dev perl-base pkg-config python3-paramiko python3-setuptools uuid-dev curl redis doxygen libical-dev python-polib gnutls-bin -y;
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -;
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 apt update;
-apt install yarn;
-cd home/;
+apt install yarn -y;
+cd ~/tools/;
 git clone https://github.com/greenbone/openvas.git;
 git clone https://github.com/greenbone/gvm-libs.git;
 cd gvm-libs;
